ac44a4f0c83d8383aa6764fce08dc56a0ad22d99
molnarbf <molnarb@tcd.ie>
2020-10-16 20:34:17 +0100

    Created test file

--- src/LCA/LCA.java
+++ src/LCA/LCA.java
@@ -1,17 +1,42 @@
 package LCA;
 
-class Node {
-	int key;
-	Node left;
-	Node right;
+public class LCA {
 
-	public Node (int item) {
-		key = item;
-		left = null;
-		right = null;
+	private class Node {
+		int key;
+		Node left;
+		Node right;
+
+		public Node (int item) {
+			key = item;
+			left = null;
+			right = null;
+		}
+	}
+
+	public static Node lowestCommonAncestor(Node root, Node a, Node b) {
+		
+		if(root == null) {
+			return null;
+		}
+		
+		// check if the root is one of the nodes, if so return root
+		if(root.key == a.key || root.key == b.key ) {
+			return root;
+		}
+		Node left = lowestCommonAncestor(root.left, a, b);
+		Node right = lowestCommonAncestor(root.right, a, b);
+
+		// if both left and right node are not null, it is the LCA
+		if(left != null && right != null) {
+			return root; 
+		} 
+		if(left == null) {
+			return right;
+		}
+		else {
+			return left;
+		}
 	}
 }
 
-public class LCA {
-	
-}
---
+++ src/LCA/LCATest.java
@@ -0,0 +1,14 @@
+package LCA;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LCATest {
+
+	@Test
+	void testLowestCommonAncestor() {
+		fail("Not yet implemented");
+	}
+
+}
