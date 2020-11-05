2f220fc5a602bb2b47f62f9fe81e91a5ec95a745
molnarbf <molnarb@tcd.ie>
2020-11-05 14:53:47 +0000

    added DAG constructor

--- src/DAG.java
+++ src/DAG.java
@@ -1,5 +1,23 @@
-// Directed Acyclic Graph implementation of finding the Lowest Common Ancestor 
+import java.util.ArrayList;
 
+/* Directed Acyclic Graph implementation of finding the Lowest Common Ancestor 
+*
+* A directed acyclic graph is a directed graph with no directed cycles, therefore you can only travel in the specified direction.
+* It is impossible to traverse the whole graph starting at one edge.
+*/
 public class DAG {
-
+	
+	private final int vertex;
+	private final ArrayList<Integer>[] next;
+	
+	// constructor
+	public DAG(int vertex) {
+		this.vertex = vertex;
+		next = (ArrayList<Integer>[]) new ArrayList[vertex];
+		
+		// Arraylist for each vertex
+		for(int i = 0; i < vertex; i++) {
+			next[i] = new ArrayList<Integer>();
+		}
+	}
 }
